1affe3a0e98472f4a87e33033750d7d3eecbe84c
ArvinCJH <jiji6143@163.com>
2019-03-13 15:36:23 +0800

    newProject

---
+++ src/com/tianwei/value/BasedValue.java
@@ -0,0 +1,18 @@
+package com.tianwei.value;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: arvin
+ * Date: 19-3-13
+ * Time: 下午3:29
+ * Description: Based on the value
+ */
+
+public class BasedValue {
+    public static final String MONGODB_IP ="192.168.2.120" ;
+    public static final String MONGODB_PORT ="27017" ;
+    public static final String MONGODB_HOST ="mongodb://"+MONGODB_IP+MONGODB_PORT ;
+    public static final String MONGODB_LOGINNAME ="27017" ;
+    public static final String MONGODB_LOGINPASS ="27017" ;
+
+}
